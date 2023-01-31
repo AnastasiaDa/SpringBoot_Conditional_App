30e88c2b38cbfb08459eb403325d10da11d91bef
AnastasiaDa <danilova12333@gmail.com>
2023-01-31 14:11:42 +0300

    docker added

--- src/test/java/ru/netology/spring_boot_conditional_app/SpringBootConditionalAppApplicationTests.java
+++ src/test/java/ru/netology/spring_boot_conditional_app/SpringBootConditionalAppApplicationTests.java
@@ -1,13 +1,49 @@
 package ru.netology.spring_boot_conditional_app;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootConditionalAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    TestRestTemplate restTemplate;
 
+    @Container
+    private static GenericContainer<?> devApp = new GenericContainer<>("devapp:latest")
+            .withExposedPorts(8080);
+
+    @Container
+    private static GenericContainer<?> prodApp = new GenericContainer<>("prodapp:latest")
+            .withExposedPorts(8081);
+
+    @BeforeAll
+    public static void setUp() {
+        devApp.start();
+        prodApp.start();
+
+    }
+
+    @Test
+    public void contextLoadsDev() {
+        ResponseEntity<String> forEntity = restTemplate
+                .getForEntity("http://localhost:" + devApp.getMappedPort(8080) + "/profile", String.class);
+        System.out.println(forEntity.getBody());
+        Assertions.assertEquals("Current profile is dev", forEntity.getBody());
+    }
+
+    @Test
+    public void contextLoadsProd() {
+        ResponseEntity<String> forEntity = restTemplate
+                .getForEntity("http://localhost:" + prodApp.getMappedPort(8081) + "/profile", String.class);
+        System.out.println(forEntity.getBody());
+        Assertions.assertEquals("Current profile is production", forEntity.getBody());
+    }
 }
